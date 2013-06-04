576a1796176d24375467c2525563214a512daab3
Josselin Perrus <josselin.perrus@gmail.com>
2013-06-04 23:33:30 +0200

    fixed a few things before last commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,10 @@ require 'will_paginate/array'
 class StaticPagesController < ApplicationController
 
 	def home
-		carousel_features_number = 10
-		#featured_breves = Breve.where("status='published'").order("random()").limit(carousel_features_number+8)
-		featured_breves = Breve.where("status='published'").order("updated_at DESC").limit(carousel_features_number+8)
-		@carousel_features = featured_breves.first(carousel_features_number)
-		@secondary_features = featured_breves.last(8)
+		carousel_features_number = 15
+		featured_breves = Breve.where("status='published'").order("random()").limit(carousel_features_number)
+		#featured_breves = Breve.where("status='published'").order("updated_at DESC").limit(carousel_features_number+8)
+		@carousel_features = featured_breves
 	end
 
 	def contribute
